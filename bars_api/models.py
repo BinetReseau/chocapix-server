b58f9c6b63c107a1b9e3bf61307e28f956a7d028
Nadrieril <nadrieril@gmail.com>
2014-08-25 19:02:54 +0200

    Added bars_api app

---
+++ bars_api/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Bar(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.id
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    login = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.name
+
+class Account(models.Model):
+    bar = models.ForeignKey(Bar)
+    owner = models.ForeignKey(User)
+    money = models.DecimalField(max_digits=7, decimal_places=3)
+
+    def __unicode__(self):
+        return self.owner.name, "("+self.bar.id+")"
