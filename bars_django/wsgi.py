07b98dcdee27780474750ff071a1efaf9993489b
Nadrieril <nadrieril@gmail.com>
2014-08-25 21:54:14 +0200

    Improved wsgi.py; mimics the behaviour of runserver command. Helps with auto-reloading of modified files

--- bars_django/wsgi.py
+++ bars_django/wsgi.py
@@ -8,8 +8,20 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))));
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))));
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bars_django.settings")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+
+
+# From http://blog.dscpl.com.au/2010/03/improved-wsgi-script-for-use-with.html
+import django.core.management
+utility = django.core.management.ManagementUtility()
+command = utility.fetch_command('runserver')
+
+command.validate()
+
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
