1fcb5ce8d5787d2db75e77fdaafba791f015a6b2
Nadrieril <nadrieril@gmail.com>
2014-08-25 20:23:35 +0200

    Added static files for the admin page

--- bars_django/settings.py
+++ bars_django/settings.py
@@ -81,3 +81,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(__file__)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
