1b25d5b641176fbf071d77813347515fe4ec54bb
Nadrieril <nadrieril@gmail.com>
2014-09-01 17:05:07 +0200

    Switched to mysql

--- bars_django/settings.py
+++ bars_django/settings.py
@@ -59,10 +59,14 @@ WSGI_APPLICATION = 'bars_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
