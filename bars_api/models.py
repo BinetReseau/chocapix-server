4597dcc9468839b9e1ba263bba8fac21e5877072
Nadrieril <nadrieril@gmail.com>
2014-09-12 03:16:31 +0200

    Added some models

--- bars_api/models.py
+++ bars_api/models.py
@@ -3,14 +3,20 @@ from django.db import models
 
 class Bar(models.Model):
     id = models.CharField(max_length=50, primary_key=True)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
+    last_modified = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return self.id
 
 class User(models.Model):
-    name = models.CharField(max_length=50)
-    login = models.CharField(max_length=50)
+    login = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=100)
+
+    name = models.CharField(max_length=100)
+    pseudo = models.CharField(max_length=50)
+
+    last_modified = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return self.name
@@ -19,6 +25,17 @@ class Account(models.Model):
     bar = models.ForeignKey(Bar)
     owner = models.ForeignKey(User)
     money = models.DecimalField(max_digits=7, decimal_places=3)
+    last_modified = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return self.owner.name + " ("+self.bar.id+")"
+
+class Item(models.Model):
+    bar = models.ForeignKey(Bar)
+    name = models.CharField(max_length=100)
+    qty = models.DecimalField(max_digits=7, decimal_places=3)
+    price = models.DecimalField(max_digits=7, decimal_places=3)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.name
