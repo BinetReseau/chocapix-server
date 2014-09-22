c0eb1ed6a00fbb61e4d14f33d4c8fa218035925d
Nadrieril <nadrieril@gmail.com>
2014-09-22 14:02:39 +0200

    Changed static url

--- bars_django/settings.py
+++ bars_django/settings.py
@@ -86,7 +86,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
 PROJECT_ROOT = os.path.dirname(__file__)
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
 
