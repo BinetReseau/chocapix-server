821524fe8b2c2ebf51a0f9abd4449dca29092253
Nadrieril <nadrieril@gmail.com>
2014-09-28 01:48:13 +0200

    Moved VirtualField declaration

--- bars_api/models.py
+++ bars_api/models.py
@@ -4,28 +4,7 @@ from rest_framework import serializers, decorators
 from rest_framework.response import Response
 from django.core.exceptions import ValidationError
 
-from rest_framework import fields
-class VirtualField(fields.Field):
-    type_name = 'VirtualField'
-    type_label = 'virtual'
-    label = 'virtual'
-    source = ''
-
-    def __init__(self, value):
-        self.value = value
-
-    # def validate(self, value):
-    #     pass
-
-    def field_to_native(self, obj, field_name):
-        return self.value
-
-    def field_from_native(self, data, files, field_name, into):
-        pass
-
-    # def from_native(self, value):
-    #     return value
-
+from bars_api import VirtualField
 
 ## User
 class User(models.Model):
