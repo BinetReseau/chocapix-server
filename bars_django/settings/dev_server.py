e88426668aa18b68503be8017cf892388b0180ac
Nadrieril <nadrieril@gmail.com>
2014-11-28 18:36:58 +0100

    Keep MySQL on server

--- bars_django/settings/dev_server.py
+++ bars_django/settings/dev_server.py
@@ -2,3 +2,13 @@ from .dev_local import *
 
 STATIC_URL = '/bars/api/static/'
 CORS_ORIGIN_ALLOW_ALL = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'root',
+    }
+}
