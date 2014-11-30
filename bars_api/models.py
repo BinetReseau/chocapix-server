4695e601a7929d6acda73067c628e3a4d2267b7e
Nadrieril <nadrieril@gmail.com>
2014-11-30 11:33:28 +0100

    Added filtering for the account list

--- bars_api/models.py
+++ bars_api/models.py
@@ -47,6 +47,19 @@ class AccountViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
 
+    def get_queryset(self):
+        queryset = Account.objects.all()
+
+        owner = self.request.QUERY_PARAMS.get('owner', None)
+        if owner is not None:
+            queryset = queryset.filter(owner=owner)
+
+        bar = self.request.QUERY_PARAMS.get('bar', None)
+        if bar is not None:
+            queryset = queryset.filter(bar=bar)
+
+        return queryset
+
     @decorators.list_route(methods=['get'])
     def me(self, request):
         # Todo: bar
