7a71c1e0734e569406bfaf986393511e74937b31
Nadrieril <nadrieril@gmail.com>
2015-01-18 20:26:14 +0100

    Added first per-object permissions

--- bars_api/perms.py
+++ bars_api/perms.py
@@ -1,5 +1,33 @@
-from permission.logics import AuthorPermissionLogic
+from permission.utils.field_lookup import field_lookup
+from permission.logics import PermissionLogic, AuthorPermissionLogic
+from bars_api.models.role import Role
+
+class BarRolePermissionLogic(PermissionLogic):
+    def __init__(self, field_name=None):
+        self.field_name = field_name or 'bar'
+
+    def has_perm(self, user, perm, obj=None):
+        if not user.is_authenticated():
+            return False
+
+        if obj is None:
+            return True
+        elif user.is_active:
+            bar = field_lookup(obj, self.field_name)
+            if bar is None:
+                return False
+            roles = Role.objects.filter(user=user, bar=bar)
+            for r in roles:
+                if perm in r.get_permissions():
+                    return True
+            return False
+
 
 PERMISSION_LOGICS = (
+    ('bars_api.Account', BarRolePermissionLogic()),
+    ('bars_api.Item', BarRolePermissionLogic()),
+    # ('bars_api.News', BarRolePermissionLogic()),
+    ('bars_api.Role', BarRolePermissionLogic()),
+    ('bars_api.Transaction', BarRolePermissionLogic()),
     ('bars_api.Transaction', AuthorPermissionLogic(field_name='author')),
 )
