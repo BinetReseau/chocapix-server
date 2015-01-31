abb570b0aa54449643629b7fd79e16933c360103
Nadrieril <nadrieril@gmail.com>
2015-01-31 11:48:25 +0100

    Compacted migrations

--- bars_api/migrations/0001_initial.py
+++ bars_api/migrations/0001_initial.py
@@ -16,12 +16,14 @@ class Migration(migrations.Migration):
             name='User',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
                 ('username', models.CharField(unique=True, max_length=128)),
-                ('password', models.CharField(unique=True, max_length=128)),
                 ('full_name', models.CharField(max_length=128, blank=True)),
                 ('pseudo', models.CharField(max_length=128, blank=True)),
-                ('last_login', models.DateTimeField(default=django.utils.timezone.now)),
+                ('is_active', models.BooleanField(default=True)),
                 ('last_modified', models.DateTimeField(auto_now=True)),
+                ('is_superuser', models.BooleanField(default=False)),
             ],
             options={
             },
@@ -31,7 +33,7 @@ class Migration(migrations.Migration):
             name='Account',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('money', models.DecimalField(max_digits=8, decimal_places=3)),
+                ('money', models.FloatField(default=0)),
                 ('last_modified', models.DateTimeField(auto_now=True)),
             ],
             options={
@@ -42,9 +44,11 @@ class Migration(migrations.Migration):
             name='AccountOperation',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('prev_value', models.DecimalField(max_digits=8, decimal_places=3)),
-                ('delta', models.DecimalField(max_digits=8, decimal_places=3)),
-                ('account', models.ForeignKey(to='bars_api.Account')),
+                ('prev_value', models.FloatField()),
+                ('fixed', models.BooleanField(default=False)),
+                ('delta', models.FloatField()),
+                ('next_value', models.FloatField()),
+                ('target', models.ForeignKey(to='bars_api.Account')),
             ],
             options={
             },
@@ -66,8 +70,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=100)),
-                ('keywords', models.CharField(max_length=200)),
-                ('qty', models.FloatField()),
+                ('keywords', models.CharField(max_length=200, blank=True)),
+                ('qty', models.FloatField(default=0)),
                 ('unit', models.CharField(max_length=100, blank=True)),
                 ('unit_value', models.FloatField(default=1)),
                 ('buy_unit', models.CharField(max_length=100, blank=True)),
@@ -75,6 +79,7 @@ class Migration(migrations.Migration):
                 ('price', models.FloatField()),
                 ('buy_price', models.FloatField(default=1)),
                 ('deleted', models.BooleanField(default=False)),
+                ('unavailable', models.BooleanField(default=False)),
                 ('last_modified', models.DateTimeField(auto_now=True)),
                 ('bar', models.ForeignKey(to='bars_api.Bar')),
             ],
@@ -87,8 +92,39 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('prev_value', models.FloatField()),
+                ('fixed', models.BooleanField(default=False)),
                 ('delta', models.FloatField()),
-                ('item', models.ForeignKey(to='bars_api.Item')),
+                ('next_value', models.FloatField()),
+                ('target', models.ForeignKey(to='bars_api.Item')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='News',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('text', models.TextField()),
+                ('timestamp', models.DateTimeField(auto_now_add=True)),
+                ('last_modified', models.DateTimeField(auto_now=True)),
+                ('deleted', models.BooleanField(default=False)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('bar', models.ForeignKey(to='bars_api.Bar')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Role',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=127)),
+                ('last_modified', models.DateTimeField(auto_now=True)),
+                ('bar', models.ForeignKey(to='bars_api.Bar')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
             },
--- scripts/dbdump.py
+++ scripts/dbdump.py
@@ -26,6 +26,7 @@
 # you must make sure ./some_folder/__init__.py exists
 # and run  ./manage.py runscript some_folder.some_script
 
+import sys, os
 from django.db import transaction
 
 class BasicImportHelper(object):
@@ -127,8 +128,6 @@ def run():
     importer.post_import()
 
 def import_data():
-    # Initial Imports
-
     # Processing model: Bar
 
     from bars_api.models.bar import Bar
@@ -136,8 +135,79 @@ def import_data():
     bars_api_bar_1 = Bar()
     bars_api_bar_1.id = u'avironjone'
     bars_api_bar_1.name = u'Aviron J\xf4ne'
-    bars_api_bar_1.last_modified = dateutil.parser.parse("2014-12-02T17:46:55+00:00")
     bars_api_bar_1 = importer.save_or_locate(bars_api_bar_1)
+
+    bars_api_bar_2 = Bar()
+    bars_api_bar_2.id = u'footrouje'
+    bars_api_bar_2.name = u'Foot Rouje'
+    bars_api_bar_2 = importer.save_or_locate(bars_api_bar_2)
+
+    bars_api_bar_3 = Bar()
+    bars_api_bar_3.id = u'natationjone'
+    bars_api_bar_3.name = u'Natation J\xf4ne'
+    bars_api_bar_3 = importer.save_or_locate(bars_api_bar_3)
+
+    # Processing model: User
+
+    from bars_api.models.user import User
+
+    bars_api_user_1 = User()
+    bars_api_user_1.username = u'nadri'
+    bars_api_user_1.set_password('nadri')
+    bars_api_user_1.full_name = u'Nadrieril'
+    bars_api_user_1.pseudo = u'Nadri'
+    bars_api_user_1 = importer.save_or_locate(bars_api_user_1)
+
+    bars_api_user_2 = User()
+    bars_api_user_2.username = u'ntag'
+    bars_api_user_2.set_password('ntag')
+    bars_api_user_2.full_name = u'Basile Bruneau'
+    bars_api_user_2.pseudo = u'NTag'
+    bars_api_user_2 = importer.save_or_locate(bars_api_user_2)
+
+    bars_api_user_3 = User()
+    bars_api_user_3.username = u'tizot'
+    bars_api_user_3.set_password('tizot')
+    bars_api_user_3.full_name = u'Camille Masset'
+    bars_api_user_3.pseudo = u'Tizot'
+    bars_api_user_3 = importer.save_or_locate(bars_api_user_3)
+
+    bars_api_user_4 = User()
+    bars_api_user_4.username = u'denis'
+    bars_api_user_4.set_password('denis')
+    bars_api_user_4.full_name = u'Denis M\xe9rigoux'
+    bars_api_user_4.pseudo = u''
+    bars_api_user_4 = importer.save_or_locate(bars_api_user_4)
+
+
+    # Processing model: Account
+
+    from bars_api.models.account import Account
+
+    bars_api_account_1 = Account()
+    bars_api_account_1.bar = bars_api_bar_1
+    bars_api_account_1.owner = bars_api_user_1
+    bars_api_account_1 = importer.save_or_locate(bars_api_account_1)
+
+    bars_api_account_2 = Account()
+    bars_api_account_2.bar = bars_api_bar_1
+    bars_api_account_2.owner = bars_api_user_2
+    bars_api_account_2 = importer.save_or_locate(bars_api_account_2)
+
+    bars_api_account_3 = Account()
+    bars_api_account_3.bar = bars_api_bar_1
+    bars_api_account_3.owner = bars_api_user_3
+    bars_api_account_3 = importer.save_or_locate(bars_api_account_3)
+
+    bars_api_account_4 = Account()
+    bars_api_account_4.bar = bars_api_bar_1
+    bars_api_account_4.owner = bars_api_user_4
+    bars_api_account_4 = importer.save_or_locate(bars_api_account_4)
+
+    bars_api_account_5 = Account()
+    bars_api_account_5.bar = bars_api_bar_3
+    bars_api_account_5.owner = bars_api_user_2
+    bars_api_account_5 = importer.save_or_locate(bars_api_account_5)
 
     # Processing model: Item
 
@@ -147,204 +217,272 @@ def import_data():
     bars_api_item_1.bar = bars_api_bar_1
     bars_api_item_1.name = u'Chocolat'
     bars_api_item_1.keywords = u''
-    bars_api_item_1.qty = 2.0
     bars_api_item_1.unit = u''
     bars_api_item_1.unit_value = 1.0
     bars_api_item_1.buy_unit = u''
     bars_api_item_1.buy_unit_value = 1.0
     bars_api_item_1.price = 1.0
     bars_api_item_1.buy_price = 1.0
-    bars_api_item_1.deleted = False
-    bars_api_item_1.last_modified = dateutil.parser.parse("2014-12-02T17:46:57+00:00")
     bars_api_item_1 = importer.save_or_locate(bars_api_item_1)
 
     bars_api_item_2 = Item()
     bars_api_item_2.bar = bars_api_bar_1
     bars_api_item_2.name = u'Pizza'
     bars_api_item_2.keywords = u''
-    bars_api_item_2.qty = -9.0
     bars_api_item_2.unit = u''
     bars_api_item_2.unit_value = 1.0
     bars_api_item_2.buy_unit = u''
     bars_api_item_2.buy_unit_value = 1.0
     bars_api_item_2.price = 2.0
     bars_api_item_2.buy_price = 1.0
-    bars_api_item_2.deleted = False
-    bars_api_item_2.last_modified = dateutil.parser.parse("2014-12-02T19:52:44+00:00")
     bars_api_item_2 = importer.save_or_locate(bars_api_item_2)
 
     bars_api_item_3 = Item()
     bars_api_item_3.bar = bars_api_bar_1
     bars_api_item_3.name = u'Tomates'
     bars_api_item_3.keywords = u'tomates'
-    bars_api_item_3.qty = 10.0
     bars_api_item_3.unit = u''
     bars_api_item_3.unit_value = 1.0
     bars_api_item_3.buy_unit = u''
     bars_api_item_3.buy_unit_value = 1.0
     bars_api_item_3.price = 1.5
     bars_api_item_3.buy_price = 1.0
-    bars_api_item_3.deleted = False
-    bars_api_item_3.last_modified = dateutil.parser.parse("2014-12-02T17:46:55+00:00")
     bars_api_item_3 = importer.save_or_locate(bars_api_item_3)
 
     bars_api_item_4 = Item()
     bars_api_item_4.bar = bars_api_bar_1
     bars_api_item_4.name = u'Nutella'
     bars_api_item_4.keywords = u'nutella tartiner'
-    bars_api_item_4.qty = 4.0
     bars_api_item_4.unit = u'g'
     bars_api_item_4.unit_value = 0.00158730159
     bars_api_item_4.buy_unit = u'pot'
     bars_api_item_4.buy_unit_value = 1.0
     bars_api_item_4.price = 8.0
     bars_api_item_4.buy_price = 8.0
-    bars_api_item_4.deleted = False
-    bars_api_item_4.last_modified = dateutil.parser.parse("2014-12-02T19:51:40+00:00")
     bars_api_item_4 = importer.save_or_locate(bars_api_item_4)
 
     bars_api_item_5 = Item()
     bars_api_item_5.bar = bars_api_bar_1
     bars_api_item_5.name = u'Spaghetti'
     bars_api_item_5.keywords = u'pates italie'
-    bars_api_item_5.qty = 10.0
-    bars_api_item_5.unit = u''
-    bars_api_item_5.unit_value = 1.0
-    bars_api_item_5.buy_unit = u''
+    bars_api_item_5.unit = u'g'
+    bars_api_item_5.unit_value = 1000.0
+    bars_api_item_5.buy_unit = u'kg'
     bars_api_item_5.buy_unit_value = 1.0
-    bars_api_item_5.price = 1.4
+    bars_api_item_5.price = 1e-06
     bars_api_item_5.buy_price = 1.0
-    bars_api_item_5.deleted = False
-    bars_api_item_5.last_modified = dateutil.parser.parse("2014-12-02T17:46:55+00:00")
     bars_api_item_5 = importer.save_or_locate(bars_api_item_5)
 
     bars_api_item_6 = Item()
     bars_api_item_6.bar = bars_api_bar_1
     bars_api_item_6.name = u'Dosette Nespresso'
     bars_api_item_6.keywords = u'caf\xe9'
-    bars_api_item_6.qty = 230.0
     bars_api_item_6.unit = u''
     bars_api_item_6.unit_value = 1.0
     bars_api_item_6.buy_unit = u''
     bars_api_item_6.buy_unit_value = 1.0
     bars_api_item_6.price = 0.48
     bars_api_item_6.buy_price = 1.0
-    bars_api_item_6.deleted = False
-    bars_api_item_6.last_modified = dateutil.parser.parse("2014-12-02T17:46:55+00:00")
     bars_api_item_6 = importer.save_or_locate(bars_api_item_6)
 
     bars_api_item_7 = Item()
     bars_api_item_7.bar = bars_api_bar_1
     bars_api_item_7.name = u'1664'
     bars_api_item_7.keywords = u'bi\xe8re'
-    bars_api_item_7.qty = 23.0
     bars_api_item_7.unit = u''
     bars_api_item_7.unit_value = 1.0
-    bars_api_item_7.buy_unit = u''
-    bars_api_item_7.buy_unit_value = 1.0
+    bars_api_item_7.buy_unit = u'Pack'
+    bars_api_item_7.buy_unit_value = 20.0
     bars_api_item_7.price = 1.2
     bars_api_item_7.buy_price = 1.0
-    bars_api_item_7.deleted = False
-    bars_api_item_7.last_modified = dateutil.parser.parse("2014-12-02T17:46:55+00:00")
     bars_api_item_7 = importer.save_or_locate(bars_api_item_7)
 
     bars_api_item_8 = Item()
     bars_api_item_8.bar = bars_api_bar_1
     bars_api_item_8.name = u'Pizza 4 fromages'
     bars_api_item_8.keywords = u'pizza'
-    bars_api_item_8.qty = 3.0
     bars_api_item_8.unit = u''
     bars_api_item_8.unit_value = 1.0
     bars_api_item_8.buy_unit = u''
     bars_api_item_8.buy_unit_value = 1.0
     bars_api_item_8.price = 3.4
     bars_api_item_8.buy_price = 1.0
-    bars_api_item_8.deleted = False
-    bars_api_item_8.last_modified = dateutil.parser.parse("2014-12-05T17:30:07+00:00")
     bars_api_item_8 = importer.save_or_locate(bars_api_item_8)
 
     bars_api_item_9 = Item()
     bars_api_item_9.bar = bars_api_bar_1
     bars_api_item_9.name = u'Pizza bolognaise'
     bars_api_item_9.keywords = u'pizza'
-    bars_api_item_9.qty = 13.0
     bars_api_item_9.unit = u''
     bars_api_item_9.unit_value = 1.0
     bars_api_item_9.buy_unit = u''
     bars_api_item_9.buy_unit_value = 1.0
     bars_api_item_9.price = 3.1
     bars_api_item_9.buy_price = 1.0
-    bars_api_item_9.deleted = False
-    bars_api_item_9.last_modified = dateutil.parser.parse("2014-12-02T17:46:55+00:00")
     bars_api_item_9 = importer.save_or_locate(bars_api_item_9)
 
     bars_api_item_10 = Item()
     bars_api_item_10.bar = bars_api_bar_1
     bars_api_item_10.name = u'Margharitta'
     bars_api_item_10.keywords = u'pizza'
-    bars_api_item_10.qty = 2.0
     bars_api_item_10.unit = u''
     bars_api_item_10.unit_value = 1.0
     bars_api_item_10.buy_unit = u''
     bars_api_item_10.buy_unit_value = 1.0
     bars_api_item_10.price = 2.8
     bars_api_item_10.buy_price = 1.0
-    bars_api_item_10.deleted = False
-    bars_api_item_10.last_modified = dateutil.parser.parse("2014-12-02T17:46:55+00:00")
     bars_api_item_10 = importer.save_or_locate(bars_api_item_10)
 
-    # Processing model: User
+    bars_api_item_11 = Item()
+    bars_api_item_11.bar = bars_api_bar_1
+    bars_api_item_11.name = u'Radis'
+    bars_api_item_11.keywords = u'l\xe9gume'
+    bars_api_item_11.unit = u'g'
+    bars_api_item_11.unit_value = 0.001
+    bars_api_item_11.buy_unit = u'kg'
+    bars_api_item_11.buy_unit_value = 1.0
+    bars_api_item_11.price = 8.0
+    bars_api_item_11.buy_price = 1.0
+    bars_api_item_11 = importer.save_or_locate(bars_api_item_11)
 
-    from bars_api.models.user import User
+    bars_api_item_12 = Item()
+    bars_api_item_12.bar = bars_api_bar_1
+    bars_api_item_12.name = u'Galette'
+    bars_api_item_12.keywords = u'sarazin'
+    bars_api_item_12.unit = u''
+    bars_api_item_12.unit_value = 0.166666666666667
+    bars_api_item_12.buy_unit = u'paquet de 6'
+    bars_api_item_12.buy_unit_value = 1.0
+    bars_api_item_12.price = 3.5
+    bars_api_item_12.buy_price = 1.0
+    bars_api_item_12 = importer.save_or_locate(bars_api_item_12)
 
-    bars_api_user_1 = User()
-    bars_api_user_1.username = u'nadri'
-    bars_api_user_1.password = u'ea5a02ec18194e1a86cf8eefd9a4132dc4b2f351a5833df531ffb914b2b6c54e2a12d6118864b945ac5a2496534eadeb15c574ae0672242798557bbbecd86c4d'
-    bars_api_user_1.full_name = u'Nadrieril'
-    bars_api_user_1.pseudo = u'Nadri'
-    bars_api_user_1.last_login = dateutil.parser.parse("2014-11-28T17:39:02+00:00")
-    bars_api_user_1.last_modified = dateutil.parser.parse("2014-12-02T17:46:55+00:00")
-    bars_api_user_1 = importer.save_or_locate(bars_api_user_1)
+    bars_api_item_13 = Item()
+    bars_api_item_13.bar = bars_api_bar_1
+    bars_api_item_13.name = u'Jambon'
+    bars_api_item_13.keywords = u'charcuterie'
+    bars_api_item_13.unit = u'tranche'
+    bars_api_item_13.unit_value = 0.166666666666667
+    bars_api_item_13.buy_unit = u'paquet'
+    bars_api_item_13.buy_unit_value = 1.0
+    bars_api_item_13.price = 2.67
+    bars_api_item_13.buy_price = 1.0
+    bars_api_item_13 = importer.save_or_locate(bars_api_item_13)
 
-    bars_api_user_2 = User()
-    bars_api_user_2.username = u'ntag'
-    bars_api_user_2.password = u'1672f03cc47e96bd91a92e07e067fa928d6daddbc2e44adc3128fe20f6ca65aa24e08772e0c623214d46a09c621c0e35037e17d53ad0d717ccf60679f125e26d'
-    bars_api_user_2.full_name = u'Basile Bruneau'
-    bars_api_user_2.pseudo = u'NTag'
-    bars_api_user_2.last_login = dateutil.parser.parse("2014-11-28T17:39:02+00:00")
-    bars_api_user_2.last_modified = dateutil.parser.parse("2014-12-02T17:46:55+00:00")
-    bars_api_user_2 = importer.save_or_locate(bars_api_user_2)
+    bars_api_item_14 = Item()
+    bars_api_item_14.bar = bars_api_bar_1
+    bars_api_item_14.name = u'Emmental rap\xe9'
+    bars_api_item_14.keywords = u'fromage'
+    bars_api_item_14.unit = u'g'
+    bars_api_item_14.unit_value = 0.002
+    bars_api_item_14.buy_unit = u'paquet de 500g'
+    bars_api_item_14.buy_unit_value = 1.0
+    bars_api_item_14.price = 6.3
+    bars_api_item_14.buy_price = 1.0
+    bars_api_item_14 = importer.save_or_locate(bars_api_item_14)
 
-    bars_api_user_3 = User()
-    bars_api_user_3.username = u'tizot'
-    bars_api_user_3.password = u'65da4a123003996c17272ca96647e52036b138cff1ed46c5dc116f15eada181e61fda94fa312a1ebd91b52b83c15ea97be0a6c9629515299aef1066ab8ef3f29'
-    bars_api_user_3.full_name = u'Camille Masset'
-    bars_api_user_3.pseudo = u'Tizot'
-    bars_api_user_3.last_login = dateutil.parser.parse("2014-11-28T17:39:02+00:00")
-    bars_api_user_3.last_modified = dateutil.parser.parse("2014-12-02T17:46:55+00:00")
-    bars_api_user_3 = importer.save_or_locate(bars_api_user_3)
+    bars_api_item_15 = Item()
+    bars_api_item_15.bar = bars_api_bar_1
+    bars_api_item_15.name = u'Pain de mie'
+    bars_api_item_15.keywords = u'd\xe9jeuner'
+    bars_api_item_15.unit = u'tranche'
+    bars_api_item_15.unit_value = 0.0476190476190476
+    bars_api_item_15.buy_unit = u'paquet de 21'
+    bars_api_item_15.buy_unit_value = 1.0
+    bars_api_item_15.price = 4.12
+    bars_api_item_15.buy_price = 1.0
+    bars_api_item_15 = importer.save_or_locate(bars_api_item_15)
 
-    # Processing model: Account
+    bars_api_item_16 = Item()
+    bars_api_item_16.bar = bars_api_bar_1
+    bars_api_item_16.name = u'Canette de coca'
+    bars_api_item_16.keywords = u'boisson'
+    bars_api_item_16.unit = u''
+    bars_api_item_16.unit_value = 0.1
+    bars_api_item_16.buy_unit = u'pack de 10'
+    bars_api_item_16.buy_unit_value = 1.0
+    bars_api_item_16.price = 5.6
+    bars_api_item_16.buy_price = 1.0
+    bars_api_item_16 = importer.save_or_locate(bars_api_item_16)
 
-    from bars_api.models.account import Account
+    bars_api_item_17 = Item()
+    bars_api_item_17.bar = bars_api_bar_1
+    bars_api_item_17.name = u'Iced tea'
+    bars_api_item_17.keywords = u'boire'
+    bars_api_item_17.unit = u'verre'
+    bars_api_item_17.unit_value = 0.1
+    bars_api_item_17.buy_unit = u'bouteille de 2l'
+    bars_api_item_17.buy_unit_value = 1.0
+    bars_api_item_17.price = 2.2
+    bars_api_item_17.buy_price = 1.0
+    bars_api_item_17 = importer.save_or_locate(bars_api_item_17)
 
-    bars_api_account_1 = Account()
-    bars_api_account_1.bar = bars_api_bar_1
-    bars_api_account_1.owner = bars_api_user_1
-    bars_api_account_1.money = -1974.00024
-    bars_api_account_1.last_modified = dateutil.parser.parse("2014-12-02T17:47:18+00:00")
-    bars_api_account_1 = importer.save_or_locate(bars_api_account_1)
+    bars_api_item_18 = Item()
+    bars_api_item_18.bar = bars_api_bar_1
+    bars_api_item_18.name = u"Jus d'orange"
+    bars_api_item_18.keywords = u'd\xe9jeuner'
+    bars_api_item_18.unit = u'verre'
+    bars_api_item_18.unit_value = 0.2
+    bars_api_item_18.buy_unit = u'l'
+    bars_api_item_18.buy_unit_value = 1.0
+    bars_api_item_18.price = 3.2
+    bars_api_item_18.buy_price = 1.0
+    bars_api_item_18 = importer.save_or_locate(bars_api_item_18)
 
-    bars_api_account_2 = Account()
-    bars_api_account_2.bar = bars_api_bar_1
-    bars_api_account_2.owner = bars_api_user_2
-    bars_api_account_2.money = 42.0730158728
-    bars_api_account_2.last_modified = dateutil.parser.parse("2014-12-05T17:30:07+00:00")
-    bars_api_account_2 = importer.save_or_locate(bars_api_account_2)
+    bars_api_item_19 = Item()
+    bars_api_item_19.bar = bars_api_bar_1
+    bars_api_item_19.name = u'Pizza mozarella'
+    bars_api_item_19.keywords = u'fromage'
+    bars_api_item_19.unit = u''
+    bars_api_item_19.unit_value = 1.0
+    bars_api_item_19.buy_unit = u''
+    bars_api_item_19.buy_unit_value = 1.0
+    bars_api_item_19.price = 3.2
+    bars_api_item_19.buy_price = 1.0
+    bars_api_item_19 = importer.save_or_locate(bars_api_item_19)
 
-    bars_api_account_3 = Account()
-    bars_api_account_3.bar = bars_api_bar_1
-    bars_api_account_3.owner = bars_api_user_3
-    bars_api_account_3.money = 1005.0
-    bars_api_account_3.last_modified = dateutil.parser.parse("2014-12-02T19:52:50+00:00")
-    bars_api_account_3 = importer.save_or_locate(bars_api_account_3)
+    # Processing model: News
+
+    from bars_api.models.news import News
+
+    bars_api_news_1 = News()
+    bars_api_news_1.bar = bars_api_bar_1
+    bars_api_news_1.author = bars_api_user_3
+    bars_api_news_1.name = u"C'est du propre !"
+    bars_api_news_1.text = u"Bar d'\xe9tage d\xe9guelasse !! Faites votre vaisselle bande de p**** !"
+    bars_api_news_1.timestamp = dateutil.parser.parse("2015-01-30T02:37:14+00:00")
+    bars_api_news_1 = importer.save_or_locate(bars_api_news_1)
+
+    bars_api_news_2 = News()
+    bars_api_news_2.bar = bars_api_bar_1
+    bars_api_news_2.author = bars_api_user_3
+    bars_api_news_2.name = u'Merci pour les photos'
+    bars_api_news_2.text = u"Bravo \xe0 Yoann qui a pris le temps de mettre toutes les photos sur les murs du bar d'\xe9tage !"
+    bars_api_news_2.timestamp = dateutil.parser.parse("2015-01-30T02:37:14+00:00")
+    bars_api_news_2 = importer.save_or_locate(bars_api_news_2)
+
+    # Processing model: Transaction
+
+    from bars_api.models.transaction import Transaction
+    from bars_api.models.transaction import AccountOperation
+    from bars_api.models.transaction import ItemOperation
+
+    for i in range(30):
+        t = Transaction()
+        t.bar = bars_api_bar_1
+        t.author = bars_api_user_2
+        t.type = u'buy'
+        t = importer.save_or_locate(t)
+
+        ao = AccountOperation()
+        ao.transaction = t
+        ao.fixed = False
+        ao.delta = -2
+        ao.target = bars_api_account_2
+        ao = importer.save_or_locate(ao)
+
+        io = ItemOperation()
+        io.transaction = t
+        io.delta = -1.0
+        io.target = bars_api_item_2
+        io = importer.save_or_locate(io)
