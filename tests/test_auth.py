45629f5ce1c00deced24773243cd5e1ac1e17085
Nadrieril <nadrieril@gmail.com>
2015-02-03 15:31:08 +0100

    Updated auth tests to new user model

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,21 +1,14 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
-from django.test.utils import override_settings
 
 class BackendTests(TestCase):
     def setUp(self):
         from bars_api.models.user import User
         User.objects.create_user("admin", "admin")
 
-    def test_authentication(self):
-        from django.contrib.auth import authenticate
-        user = authenticate(password="admin")
-        self.assertIsNotNone(user)
-        self.assertEqual(user.username, "admin")
-
     def test_login(self):
         client = APIClient(enforce_csrf_checks=True)
-        data = {'password':"admin"}
+        data = {'password': 'admin', 'username': 'admin'}
         response = client.post('/api-token-auth/', data, format='json')
 
         self.assertEqual(response.status_code, 200)
