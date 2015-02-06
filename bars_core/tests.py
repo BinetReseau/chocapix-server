7534057e9e7d245672b5faf3f7acb47042457003
Nadrieril <nadrieril@gmail.com>
2015-02-06 01:43:40 +0100

    Cleand auth tests

--- bars_core/tests.py
+++ bars_core/tests.py
@@ -1,21 +1,19 @@
-from django.test import TestCase
-from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
+from bars_core.models.user import User
 
-class BackendTests(TestCase):
+class BackendTests(APITestCase):
     def setUp(self):
-        from bars_core.models.user import User
         User.objects.create_user("admin", "admin")
 
     def test_login(self):
-        client = APIClient(enforce_csrf_checks=True)
         data = {'password': 'admin', 'username': 'admin'}
-        response = client.post('/api-token-auth/', data, format='json')
+        response = self.client.post('/api-token-auth/', data, format='json')
 
         self.assertEqual(response.status_code, 200)
 
         token = response.data["token"]
         auth = 'JWT {0}'.format(token)
-        response = client.get('/user/me/', HTTP_AUTHORIZATION=auth, format='json')
+        response = self.client.get('/user/me/', HTTP_AUTHORIZATION=auth, format='json')
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['username'], "admin")
