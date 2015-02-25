ebf99ad4b83e5b648892a44ef5a83135ccb0defa
Nadrieril <nadrieril@gmail.com>
2015-02-25 19:39:05 +0100

    Add /sellitem/<id>/merge route

--- bars_items/models/sellitem.py
+++ bars_items/models/sellitem.py
@@ -1,5 +1,7 @@
+from django.http import Http404
 from django.db import models
-from rest_framework import viewsets, serializers, permissions
+from rest_framework import viewsets, serializers, permissions, decorators, exceptions
+from rest_framework.response import Response
 
 from bars_django.utils import VirtualField, CurrentBarCreateOnlyDefault
 from bars_core.models.bar import Bar
@@ -50,8 +52,39 @@ class SellItemSerializer(serializers.ModelSerializer):
     fuzzy_price = serializers.FloatField(read_only=True, source='calc_price')
 
 
+
+
+
+class AddStockItemSerializer(serializers.BaseSerializer):
+    sellitem = serializers.PrimaryKeyRelatedField(queryset=SellItem.objects.all())
+    unit_factor = serializers.FloatField()
+
 class SellItemViewSet(viewsets.ModelViewSet):
     queryset = SellItem.objects.all()
     serializer_class = SellItemSerializer
     permission_classes = (permissions.AllowAny,)  # TODO: temporary
     filter_fields = ['bar']
+
+    @decorators.detail_route(methods=['put'])
+    def merge(self, request, pk=None):
+        unsrz = AddStockItemSerializer(data=request.data)
+        unsrz.is_valid(raise_exception=True)
+        other = unsrz.validated_data['sellitem']
+        unit_factor = unsrz.validated_data['unit_factor']
+
+        try:
+            this = SellItem.objects.get(pk=pk)
+        except SellItem.DoesNotExist:
+            raise Http404('SellItem (id=%d) does not exist' % pk)
+
+        if this.bar != request.bar or other.bar != this.bar or other.bar != request.bar:
+            raise exceptions.PermissionDenied('Cannot operate across bars')
+
+        for stockitem in other.stockitems.all():
+            stockitem.sellitem = this
+            stockitem.unit_factor *= unit_factor
+            stockitem.save()
+        other.delete()
+
+        srz = SellItemSerializer(this)
+        return Response(srz.data, 200)
