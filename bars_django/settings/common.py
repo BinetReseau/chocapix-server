2322feaa0659e6b816574157d002528d212dc57d
Nadrieril <nadrieril@gmail.com>
2015-03-03 16:13:37 +0100

    Installed debug toolbar

--- bars_django/settings/common.py
+++ bars_django/settings/common.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'debug_toolbar',
     'rest_framework',
     'corsheaders',
     'permission',
