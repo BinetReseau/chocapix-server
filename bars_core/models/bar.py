c6523d4ff14a0ab7bc52f36dcd2f783f99ceb246
Nadrieril <nadrieril@gmail.com>
2015-03-05 23:23:44 +0100

    Add simple agios calculation

--- bars_core/models/bar.py
+++ bars_core/models/bar.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from mock import Mock
 from django.db import models
 from rest_framework import viewsets, serializers, permissions
 from bars_django.utils import VirtualField
@@ -18,6 +20,19 @@ class Bar(models.Model):
         return self.id
 
     def apply_agios(self, account):
+        if account.money >= 0 and account.overdrawn_since is not None:
+            account.overdrawn_since = None
+            account.save()
+
+        elif account.money < 0:
+            if account.overdrawn_since is None:
+                account.overdrawn_since = datetime.now()
+                account.save()
+
+            delta = abs(account.money) * 0.05
+            makeAgiosTransaction(self, account, delta)
+            return delta
+
         return 0
 
 
@@ -31,3 +46,16 @@ class BarViewSet(viewsets.ModelViewSet):
     queryset = Bar.objects.all()
     serializer_class = BarSerializer
     permission_classes = (permissions.AllowAny,)  # TODO: temporary
+
+
+def makeAgiosTransaction(bar, account, amount):
+    from bars_transactions.serializers import AgiosTransactionSerializer
+    from bars_core.models.user import get_default_user
+    user = get_default_user()
+    user.has_perm = Mock(return_value=True)
+    data = {'type': 'agios', 'account': account.id, 'amount': amount}
+    context = {'request': Mock(bar=bar, user=user)}
+
+    s = AgiosTransactionSerializer(data=data, context=context)
+    s.is_valid(raise_exception=True)
+    s.save()
