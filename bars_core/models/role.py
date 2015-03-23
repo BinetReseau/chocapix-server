84510a13741284acf80997fce1be16f7d8eedb3f
Nadrieril <nadrieril@gmail.com>
2015-03-23 02:43:20 +0100

    Refactor existing roles

--- bars_core/models/role.py
+++ bars_core/models/role.py
@@ -6,89 +6,22 @@ from rest_framework.response import Response
 from bars_django.utils import VirtualField, permission_logic
 from bars_core.models.bar import Bar
 from bars_core.models.user import User
-from bars_core.perms import BarRolePermissionLogic
+from bars_core.perms import PerBarPermissionsOrAnonReadOnly, BarRolePermissionLogic
 
-
-role_map = {}
-role_map['customer'] = [
-    'bars_transactions.add_transaction',
-    'bars_transactions.add_buytransaction',
-    'bars_transactions.add_throwtransaction',
-    'bars_transactions.add_givetransaction',
-    'bars_transactions.add_mealtransaction',
-    'bars_bugtracker.add_bugreport',
-]
-role_map['agios_daemon'] = [
-    'bars_transactions.add_transaction',
-    'bars_transactions.add_agiostransaction',
-]
-role_map['newsmanager'] = [
-    'bars_news.add_news',
-    'bars_news.change_news',
-    'bars_news.delete_news',
-]
-role_map['inventorymanager'] = [
-    'bars_transactions.add_transaction',
-    'bars_transactions.add_inventorytransaction',
-]
-role_map['policeman'] = [
-    'bars_transactions.add_transaction',
-    'bars_transactions.add_punishtransaction',
-]
-role_map['accountmanager'] = [
-    'bars_core.add_account',
-    'bars_core.change_account',
-]
-role_map['appromanager'] = role_map['inventorymanager'] + [
-    'bars_transactions.add_transaction',
-    'bars_transactions.add_approtransaction',
-
-    'bars_items.add_buyitemprice',
-    'bars_items.change_buyitemprice',
-    'bars_items.add_sellitem',
-    'bars_items.change_sellitem',
-    'bars_items.add_stockitem',
-    'bars_items.change_stockitem',
-]
-
-role_map['moneymanager'] = \
-    role_map['appromanager'] + \
-    role_map['inventorymanager'] + [
-        'bars_transactions.add_transaction',
-        'bars_transactions.add_deposittransaction',
-        'bars_transactions.add_withdrawtransaction',
-        'bars_transactions.add_refundtransaction',
-]
-role_map['staff'] = \
-    role_map['moneymanager'] + \
-    role_map['policeman'] + [
-        'bars_transactions.change_transaction',
-        'bars_transactions.add_barInvestmenttransaction',
-        'bars_transactions.add_collectivePaymenttransaction',
-        'bars_bugtracker.change_bugreport',
-]
-role_map['admin'] = \
-    role_map['staff'] + \
-    role_map['newsmanager'] + \
-    role_map['accountmanager'] + [
-        'bars_core.add_role',
-        'bars_core.change_role',
-        'bars_core.delete_role',
-]
-role_list = list(role_map.keys())
+from bars_core.roles import roles_map, roles_list
 
 
 @permission_logic(BarRolePermissionLogic())
 class Role(models.Model):
     class Meta:
         app_label = 'bars_core'
-    name = models.CharField(max_length=127, choices=zip(role_list, role_list))
+    name = models.CharField(max_length=127, choices=zip(roles_list, roles_list))
     bar = models.ForeignKey(Bar)
     user = models.ForeignKey(User)
     last_modified = models.DateTimeField(auto_now=True)
 
     def get_permissions(self):
-        return sorted(set(role_map[self.name])) if self.name in role_map else []
+        return sorted(set(roles_map[self.name])) if self.name in role_map else []
 
     def __unicode__(self):
         return self.user.username + " : " + self.name + " (" + self.bar.id + ")"
@@ -100,8 +33,6 @@ class RoleSerializer(serializers.ModelSerializer):
     _type = VirtualField("Role")
     perms = serializers.ListField(child=serializers.CharField(max_length=127), read_only=True, source='get_permissions')
 
-
-from bars_core.perms import PerBarPermissionsOrAnonReadOnly
 
 class RoleViewSet(viewsets.ModelViewSet):
     queryset = Role.objects.all()
