2dd2b808d3b088fedaafd16eb425f0947a77bfd0
Nadrieril <nadrieril@gmail.com>
2015-03-29 17:19:26 +0200

    Remove DEBUG=True in prod settings

--- bars_django/settings/prod.py
+++ bars_django/settings/prod.py
@@ -1,11 +1,7 @@
 from .common import *
 
-# TODO : temporary
-DEBUG = True
-TEMPLATE_DEBUG = True
-# REST_FRAMEWORK['DEFAULT_PERMISSION_CLASSES'] = [
-#     'rest_framework.permissions.AllowAny',
-# ]
+DEBUG = False
+TEMPLATE_DEBUG = False
 
 DATABASES = {
     'default': {
